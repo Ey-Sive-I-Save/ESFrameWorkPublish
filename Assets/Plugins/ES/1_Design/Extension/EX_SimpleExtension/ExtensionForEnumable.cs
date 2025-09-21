5828c2d5df71460283022e9787adb3258a712b02
0everey <2650026906@qq.com>
2025-09-21 15:46:43 +0800

    IENUMABLE1

--- Assets/Plugins/ES/1_Design/Extension/EX_SimpleExtension/ExtensionForEnumable.cs
+++ Assets/Plugins/ES/1_Design/Extension/EX_SimpleExtension/ExtensionForEnumable.cs
@@ -7,81 +7,101 @@ using UnityEngine;
 namespace ES
 {
 
-    public static class ExtensionForEnumable 
+    public static class ExtensionForEnumable
     {
-        // 21. 随机获取数组元素
-        public static T _RandomItem<T>(this T[] array)
+
+        #region 快捷功能
+        /// <summary>
+        /// 随机元素
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="ifNullOrEmpty">如果为空</param>
+        /// <returns></returns>
+        public static T _RandomItem<T>(this T[] array, T ifNullOrEmpty = default)
         {
-            if (array == null || array.Length == 0) return default;
+            if (array == null || array.Length == 0) return ifNullOrEmpty;
             return array[UnityEngine.Random.Range(0, array.Length)];
         }
-
-        // 22. 随机获取列表元素
-        public static T _RandomItem<T>(this List<T> list)
+        /// <summary>
+        /// 随机元素
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="ifNullOrEmpty">如果为空</param>
+        /// <returns></returns>
+        public static T _RandomItem<T>(this List<T> list, T ifNullOrEmpty = default)
         {
-            if (list == null || list.Count == 0) return default;
+            if (list == null || list.Count == 0) return ifNullOrEmpty;
             return list[UnityEngine.Random.Range(0, list.Count)];
         }
-
-        // 23. 打乱列表顺序
+        /// <summary>
+        /// 打乱列表顺序（原地修改）
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
         public static void _RandomShuffle<T>(this List<T> list)
         {
+            // 边界条件检查：数组为空或只有一个元素时无需操作
+            if (list == null || list.Count <= 1)
+            {
+                return;
+            }
             for (int i = 0; i < list.Count; i++)
             {
                 int randomIndex = UnityEngine.Random.Range(i, list.Count);
                 (list[i], list[randomIndex]) = (list[randomIndex], list[i]);
             }
         }
+        /// <summary>
+        /// 打乱数组顺序（原地修改）
+        /// </summary>
+        /// <typeparam name="T">数组元素类型</typeparam>
+        /// <param name="array">要打乱的数组</param>
+        public static void _RandomShuffle<T>(this T[] array)
+        {
+            // 边界条件检查：数组为空或只有一个元素时无需操作
+            if (array == null || array.Length <= 1)
+            {
+                return;
+            }
 
-        // 24. 检查数组是否为空或null
+            // Fisher-Yates 洗牌算法：从前向后迭代
+            for (int i = 0; i < array.Length; i++)
+            {
+                // 生成一个 [i, array.Length) 范围内的随机索引
+                // UnityEngine.Random.Range 对于整数是 [minInclusive, maxExclusive)
+                int randomIndex = UnityEngine.Random.Range(i, array.Length);
+
+                // 使用元组交换元素：简洁高效
+                (array[i], array[randomIndex]) = (array[randomIndex], array[i]);
+            }
+        }
+        /// <summary>
+        /// 是空或者无元素的数组？
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <returns></returns>
         public static bool _IsNullOrEmpty<T>(this T[] array)
         {
             return array == null || array.Length == 0;
         }
-
-        // 25. 检查列表是否为空或null
+        /// <summary>
+        /// 是空或者无元素的列表？
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
         public static bool _IsNullOrEmpty<T>(this List<T> list)
         {
             return list == null || list.Count == 0;
         }
 
-        // 26. 遍历数组并执行操作
-        public static void _ForEach<T>(this T[] array, Action<T> action)
-        {
-            if (array == null || action == null) return;
-            foreach (var item in array) action(item);
-        }
+        #endregion
 
-        // 27. 遍历列表并执行操作
-        public static void _ForEach<T>(this List<T> list, Action<T> action)
-        {
-            if (list == null || action == null) return;
-            foreach (var item in list) action(item);
-        }
 
-        // 28. 查找满足条件的第一个元素的索引
-        public static int _FindIndex<T>(this T[] array, Predicate<T> predicate)
-        {
-            if (array == null || predicate == null) return -1;
-            for (int i = 0; i < array.Length; i++)
-                if (predicate(array[i])) return i;
-            return -1;
-        }
-
-        // 29. 查找满足条件的最后一个元素的索引
-        public static int _FindLastIndex<T>(this T[] array, Predicate<T> predicate)
-        {
-            if (array == null || predicate == null) return -1;
-            for (int i = array.Length - 1; i >= 0; i--)
-                if (predicate(array[i])) return i;
-            return -1;
-        }
-
-        // 30. 检查数组是否包含满足条件的元素
-        public static bool _ContainsFeetCondition<T>(this T[] array, Predicate<T> predicate)
-        {
-            return array._FindIndex(predicate) != -1;
-        }
+      
     }
 }
 
